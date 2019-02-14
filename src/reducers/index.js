e6844304de1c79b421db33cd3f02d92519e8f6c8
Erik Saulnier <info@eriksaulnier.com>
2019-02-14 16:24:12 -0500

    Refactored project to have better structure

--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import roomReducer from './RoomReducers';
-
-export default combineReducers({
-    roomReducer
-});
--- src/utils/Socket.js
+++
@@ -1,9 +0,0 @@
-import io from 'socket.io-client';
-
-const socket = io.connect(process.env.REACT_APP_SERVER_IP);
-
-function subscribeToMessages(cb) {
-    socket.on('message', (user, message) => cb(`${user}: ${message}`));
-}
-
-export { socket, subscribeToMessages }; 
