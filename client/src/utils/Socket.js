46a0f1d4a57c43812b5dea2229f3c36ec54239b0
Erik Saulnier <info@eriksaulnier.com>
2019-03-13 00:39:13 -0400

    Hooked up automatic reconnection to the room

--- client/src/utils/Socket.js
+++ client/src/utils/Socket.js
@@ -1,13 +1,29 @@
 import io from 'socket.io-client';
 import * as ActionTypes from '../actions/Types';
+import { joinRoom } from '../actions/RoomActions';
+import configureStore from '../store';
 
-const socket = io.connect();
+const socket = io.connect(null, {
+    reconnection: true,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax : 5000,
+    reconnectionAttempts: Infinity
+});
 
 export const init = (store) => {
     Object.keys(ActionTypes).forEach((type) => {
         socket.on(type, (payload) => {
           store.dispatch({ type, payload });
         });
+    });
+
+    // handle reconnecting to rooms
+    socket.on('connect', function() {
+        const store = configureStore();
+        const state = store.getState();
+        if (state.roomReducer.room) {
+            store.dispatch(joinRoom(state.roomReducer.room));
+        }
     });
 };
 
