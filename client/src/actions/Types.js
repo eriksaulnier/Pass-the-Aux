7a50a8c9d5a5b99958731dbbc25d10cbed6d40a7
Benta63 <noahstolz@yahoo.com>
2019-03-21 10:59:52 -0400

    Implemented reset queue

--- client/src/actions/Types.js
+++ client/src/actions/Types.js
@@ -7,4 +7,5 @@ export const ADD_SONG = 'ADD_SONG';
 export const REMOVE_SONG = 'REMOVE_SONG';
 export const VOTE_SONG = 'VOTE_SONG';
 export const UPDATE_QUEUE = 'UPDATE_QUEUE';
+
 export const RESET_QUEUE = 'RESET_QUEUE';
