1f69be8b2a5a7570f3e5ccfdf98535a44415150b
Erik Saulnier <info@eriksaulnier.com>
2019-04-22 16:13:17 -0400

    Fixed bug I accidentally created yesterday

--- client/src/components/RoomPage/index.js
+++ client/src/components/RoomPage/index.js
@@ -18,7 +18,7 @@ class RoomPage extends Component {
     super(props);
 
     // setup spotify api connection if no access code is present
-    if (!this.props.isRoomOwner) {
+    if (!this.props.loggedIn) {
       this.props.connectSpotify();
     }
 
@@ -84,6 +84,7 @@ class RoomPage extends Component {
 const mapStateToProps = state => {
   return {
     room: state.roomReducer.room,
+    loggedIn: state.spotifyReducer.loggedIn,
     isRoomOwner: state.spotifyReducer.isRoomOwner
   };
 };
